b6f5ce444831268ea9ca0bac60bf9b48c6979dad
sebagruia <blackthorus@gmail.com>
2023-04-04 13:49:38 +0300

    checking build fail

--- pages/question/[id].tsx
+++ pages/question/[id].tsx
@@ -188,6 +188,7 @@ export async function getStaticPaths() {
     const questionsParams = Object.keys(questions).map((item) => {
       return { params: { id: item } };
     });
+    console.log(questionsParams);
     return {
       paths: questionsParams,
       fallback: false,
