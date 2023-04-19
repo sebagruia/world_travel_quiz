0753503987f9cbb0e51a1ceaa2a5acbfd59156b8
sebagruia <blackthorus@gmail.com>
2023-04-19 17:21:43 +0300

    Updated app for Static Site Generation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,22 @@
 import '@/styles/globals.css';
+
 import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '@/apollo/client';
 
 import { Inter } from 'next/font/google';
-
-import client from '@/apollo/client';
 
 const inter = Inter({ subsets: ['latin'] });
 
 import type { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const client = useApollo(pageProps);
   return (
     <ApolloProvider client={client}>
       <main className={inter.className}>
         <Component {...pageProps} />
       </main>
+      //{' '}
     </ApolloProvider>
   );
 };
